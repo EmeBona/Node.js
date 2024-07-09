6e5e35c41e69c2f643086b14d21b7c1d67b8a35c
María Bona <mariabonaenguita@gmail.com>
2024-07-09 10:18:09 +0200

    first exercises uploaded

---
+++ ECMAScriptModule/welcome.js
@@ -0,0 +1,5 @@
+function welcome(name){
+    console.log(`Hello, ${name}`);
+}
+
+export default welcome
---
+++ sumScript/sum.js
@@ -0,0 +1,5 @@
+const num1 = 6;
+const num2 = 5;
+const sum = num1 + num2;
+
+console.log(`sum of ${num1} + ${num2} is ${sum}`);
